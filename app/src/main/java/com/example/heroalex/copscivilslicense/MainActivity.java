df0391993d22fc73b721a8817368092ea16547c7
alexDorni <alexandrudornescu@yahoo.com>
2017-11-21 15:34:42 +0200

    New Layout register_log

--- app/src/main/java/com/example/heroalex/copscivilslicense/MainActivity.java
+++ app/src/main/java/com/example/heroalex/copscivilslicense/MainActivity.java
@@ -7,6 +7,7 @@ import android.content.DialogInterface;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AutoCompleteTextView;
@@ -15,7 +16,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
     //button civil & cop
     private Button mButtonCivil = null;
@@ -78,18 +79,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Context ct = getApplicationContext();
                 int duration = Toast.LENGTH_SHORT;
-                Toast t = Toast.makeText(ct, "Hoho a intrat", duration);
-                t.show();
+                Toast.makeText(ct, "Trebuie facut cu uitatul parolei", duration).show();
             }
         });
 
         txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Context ct = getApplicationContext();
-                int duration = Toast.LENGTH_SHORT;
-                Toast t = Toast.makeText(ct, "Hoho a intrat pe 2", duration);
-                t.show();
+
             }
         });
 
@@ -105,7 +102,21 @@ public class MainActivity extends AppCompatActivity {
 
     // TODO asta
     private void validateFields(){
+        String email = mFieldEmail.getText().toString().trim();
+        String password = mFieldPassword.getText().toString().trim();
 
+        if (TextUtils.isEmpty(email)){
+
+            Toast.makeText(this, "Please enter the email", Toast.LENGTH_SHORT);
+            return;
+        }
+        if (TextUtils.isEmpty(password)){
+
+            Toast.makeText(this, "Please insert the password", Toast.LENGTH_SHORT);
+            return;
+        }
+
+        //Verificare cu firebase
     }
 
 
